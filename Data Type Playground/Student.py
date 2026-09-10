71c2bb7402bb5310aae474adca9694d215f0ac7c
Tech-Dahlia <158784731+Tech-Dahlia@users.noreply.github.com>
2026-09-10 22:50:14 +0200

    Create Student.py for user data collection
    Added a script to collect and process user data, including first name, surname, age, and favorite number.

---
+++ Data Type Playground/Student.py
@@ -0,0 +1,15 @@
+"""
+Project: Student Information Profile Card
+Author: Dahlia Mphalo
+Description: This script collects and processes user data, demonstrating 
+mastery of string manipulation, float rounding, and data type casting.
+"""
+
+First_name = input("What's your First name?")
+Surname = input("What's your Surname?")
+age = int(input("How old are you?")) # Cast to integer immediately
+Favourite_Number = float(input("What's your favourite number?"))
+months = age * 12
+
+print( f"Welcome, {First_name.upper()} {Surname.title()}, you have lived for {months} months thus far. Could your favourite number be {Favourite_Number:.2f}? when rounded to two decimal places.   "  )
+
