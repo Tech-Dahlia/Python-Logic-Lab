d7a46b6b561d48fade37a33d266987824d74b403
Tech-Dahlia <158784731+Tech-Dahlia@users.noreply.github.com>
2026-09-15 13:08:44 +0200

    Add basic calculator functionality
    Implemented basic arithmetic operations: addition, subtraction, multiplication, and division.

--- Data Type Playground/Calculator.py
+++ Data Type Playground/Calculator.py
@@ -1,1 +1,12 @@
+#Basic Calculator
 
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+
+sum = float(a) + float(b)
+sub = float(a) - float(b)
+div = float(a) / float(b)
+Mult = float(a) * float(b)
+
+pr
