83e833794f2a1a9b9fc985b993c6f22051f3fe71
Tech-Dahlia <158784731+Tech-Dahlia@users.noreply.github.com>
2026-09-18 00:54:29 +0200

    Create Ticketbreeze.py for ticket booking
    Starting with the backbone the project...

---
+++ Data Type Playground/Ticketbreeze.py
@@ -0,0 +1,11 @@
+"""Funfact maybe not so fun:  This project is inspired by the clicks beauty fair playground i was on the que on time when the 
+tickects went live but instad of joining the que immediately after clicking join que the page loaded for a while my page
+as a result i didn't get the ticket im yet to find out if it was a site issue a web issue as i delve deeper in my coding/programming 
+career"""
+
+
+name = input("Please enter your name: ")
+artist= input("Please enter the name of the artist you want to see: ")
+
+
+print(f" Hi {name}, Your tickets to see{artist} have been booked successfully. Enjoy your show!")
