2b538590a6f4d6142e80e7af3e58edd53674adee
Tech-Dahlia <158784731+Tech-Dahlia@users.noreply.github.com>
2026-09-25 19:18:28 +0200

    Add Encryption.py for password hint functionality

---
+++ String Magic/Encryption.py
@@ -0,0 +1,9 @@
+"""
+Project: 
+Author: Dahlia Mphalo
+Description: 
+"""
+
+Password = input("Please Enter your password ").strip()
+
+hint = print( f"your passwrord starts with {Password[0].upper()} and ends with {Password[-1].upper() }" )
