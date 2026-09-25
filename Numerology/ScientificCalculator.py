e3a88ac04eb4e677b743f0f259f298cbc586a01b
Tech-Dahlia <158784731+Tech-Dahlia@users.noreply.github.com>
2026-09-25 21:00:20 +0200

    Add ScientificCalculator.py with basic operations
    Implement a multi-function scientific calculator that performs basic and advanced arithmetic operations, handles user input safely, and displays results in a formatted table. I guesss we can say this is a continuation of the previous advanced calculator but time time I learnt we ween to accoout for error like what happens when we need to divide by 0 How will we handle that find out as I learn

---
+++ Numerology/ScientificCalculator.py
@@ -0,0 +1,17 @@
+"""
+Project: Multi-Function Calculator
+Author: Dahlia Mphalo
+Description: This Python calculator called Scientific calculator.py that takes two numbers as input and performs all four basic 
+             arithmetic operations plus two advanced operations. The calculator must handle user input safely using type 
+             casting and display results clearly using f-strings.
+
+Requirements
+Use float(input()) to collect two numbers from the user
+Calculate and display: addition, subtraction, multiplication, division
+Calculate and display: floor division (//) and modulus (%)
+Round all results to 2 decimal places using round()
+Handle division by zero — if the second number is 0, display a friendly error message instead of crashing
+Display all results in a formatted table using f-strings
+"""
+
+ 
